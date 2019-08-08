b692311d545e55a253dc9714340240a435c8ba03
Dominic Tremblay <dominictremblay24@gmail.com>
2019-08-08 02:11:21 -0400

    chat client is connecting to server

--- src/App.js
+++ src/App.js
@@ -7,6 +7,30 @@ import messages from './lib/messages';
 
 const App = () => {
   const [state, setState] = useState(messages);
+  const [socketServer, setSocketServer] = useState(null);
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    setSocketServer(new WebSocket('ws://localhost:8000'));
+  }, []);
+
+  useEffect(() => {
+    if (connected) {
+      socketServer.onopen = event => {
+        console.log('Client connected to socket server');
+      };
+    }
+    // return () => (socketServer.onopen = null);
+  }, [connected, socketServer]);
+
+  useEffect(() => {
+    if (connected) {
+      socketServer.onmessage = event => {
+        const incomingMessage = JSON.parse(event.data);
+        // this.updateMessages(incomingMessage);
+      };
+    }
+  }, [connected, socketServer]);
 
   const sendMessage = () => {};
 
