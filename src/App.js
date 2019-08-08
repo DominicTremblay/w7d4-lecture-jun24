5126fa5cc633e06620e9d07564791980330ef3d6
Dominic Tremblay <dominictremblay24@gmail.com>
2019-08-08 09:09:29 -0400

    reset for demo

--- src/App.js
+++ src/App.js
@@ -5,94 +5,17 @@ import ChatBar from './ChatBar';
 import MessageList from './MessageList';
 import messages from './lib/messages';
 
-const messageReducer = (state, action) => {
-  const messageTypes = {
-    incomingMessage: {
-      ...state,
-      messages: [...state.messages, action.message],
-    },
-    incomingNotification: {
-      ...state,
-      messages: [...state.messages, action.message],
-    },
-    updateUser: {
-      ...state,
-      currentUser: {
-        ...state.currentUser,
-        name: action.username,
-      },
-    },
-  };
-
-  if (!messageTypes[action.type]) {
-    throw new Error('Unknown message type');
-  }
-
-  return messageTypes[action.type];
-};
-
-const useSocket = url => {
-  const [state, dispatch] = useReducer(messageReducer, messages);
-  const [socketServer, setSocketServer] = useState(null);
-  const [connected, setConnected] = useState(false);
-
-  useEffect(() => {
-    setSocketServer(new WebSocket(url));
-    setConnected(true);
-  }, [url]);
-
-  useEffect(() => {
-    if (connected) {
-      socketServer.onopen = event => {
-        console.log('Client connected to socket server');
-      };
-    }
-    // return () => (socketServer.onopen = null);
-  }, [connected, socketServer]);
-
-  useEffect(() => {
-    console.log('SocketServer', socketServer);
-
-    if (connected) {
-      socketServer.onmessage = event => {
-        const incomingMessage = JSON.parse(event.data);
-        console.log('Client reveived: ', incomingMessage);
-        dispatch({ type: incomingMessage.type, message: incomingMessage });
-      };
-    }
-    if (connected) {
-      return () => (socketServer.onmessage = null);
-    }
-  }, [connected, socketServer]);
-
-  return {
-    state,
-    socketServer,
-    dispatch,
-  };
-};
-
 const App = () => {
-  const { state, socketServer, dispatch } = useSocket('ws://localhost:8000');
+  // socket url is ws://localhost:8000
+
+  const [state, setState] = useState(messages);
 
   const sendMessage = msg => {
-    console.log(msg);
-    const newMessage = {
-      type: 'postMessage',
-      content: msg,
-      username: state.currentUser.name,
-    };
-    socketServer.send(JSON.stringify(newMessage));
+    console.log(`new message: ${msg}`);
   };
 
   const updateUser = username => {
-    console.log(username);
-    const newNotification = {
-      type: 'postNotification',
-      content: `${state.currentUser.name} has hanged their name to ${username}`,
-    };
-    dispatch({ type: 'updateUser', username });
-    socketServer.send(JSON.stringify(newNotification));
+    console.log(`new username: ${username}`);
   };
 
   return (
